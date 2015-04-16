6ab294bd4ac5b34a8c5620c4823767fdf629cf2e
borenbanner <borenbanner@126.com>
2015-04-16 11:03:35 +0800

    ad

---
+++ aiwo-weixin/src/com/aiwo/servlet/AddrServlet.java
@@ -0,0 +1,102 @@
+package com.aiwo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.aiwo.server.service.AddrService;
+
+public class AddrServlet extends HttpServlet {
+
+	private AddrService addrService = new AddrService() ;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7407920845449260575L;
+
+	/**
+	 * Constructor of the object.
+	 */
+	public AddrServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 * 
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	@SuppressWarnings("unchecked")
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		Map<String,String[]> params = request.getParameterMap() ;
+		Map<String,String> param = new HashMap<String,String>();
+		for(Map.Entry<String, String[]> p : params.entrySet()){
+			
+			param.put(p.getKey(),p.getValue()[0]) ;
+		}
+		
+		this.addrService.insertAddr(param);
+		
+		PrintWriter pw = response.getWriter() ;
+		pw.write("afae") ;
+		pw.close() ;
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 * 
+	 * @throws ServletException
+	 *             if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
